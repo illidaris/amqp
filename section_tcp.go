89bcdface6c76b2e269bc72d97ad49a8b87bf37b
Joseph <wangjianhua@ztgame.com>
2022-01-25 18:22:45 +0800

    set tcp info

--- section_tcp.go
+++ section_tcp.go
@@ -8,20 +8,40 @@ type TCPSection struct {
 	path string
 }
 
+func (m *TCPSection) SetHost(v string) {
+	m.host = v
+}
+
 func (m *TCPSection) GetHost() string {
 	return m.host
+}
+
+func (m *TCPSection) SetPort(v int32) {
+	m.port = v
 }
 
 func (m *TCPSection) GetPort() int32 {
 	return m.port
 }
 
+func (m *TCPSection) SetUser(v string) {
+	m.user = v
+}
+
 func (m *TCPSection) GetUser() string {
 	return m.user
 }
 
+func (m *TCPSection) SetPwd(v string) {
+	m.pwd = v
+}
+
 func (m *TCPSection) GetPwd() string {
 	return m.pwd
+}
+
+func (m *TCPSection) SetPath(v string) {
+	m.path = v
 }
 
 func (m *TCPSection) GetPath() string {
