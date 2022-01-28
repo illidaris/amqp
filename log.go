923a1a9498fe45d109ce2aad436d5d916f69af68
Joseph <wangjianhua@ztgame.com>
2022-01-28 12:15:33 +0800

    add def log

--- log.go
+++ log.go
@@ -6,6 +6,7 @@ import (
 )
 
 var logger ILogger
+var isShowLogger bool
 
 type ILogger interface {
 	DebugCtxf(ctx context.Context, format string, args ...interface{})
@@ -17,20 +18,32 @@ type ILogger interface {
 type defaultLogger struct{}
 
 func (l *defaultLogger) DebugCtxf(ctx context.Context, format string, args ...interface{}) {
-	println(fmt.Sprintf(format, args...))
+	if isShowLogger {
+		println(fmt.Sprintf(format, args...))
+	}
 }
 func (l *defaultLogger) InfoCtxf(ctx context.Context, format string, args ...interface{}) {
-	println(fmt.Sprintf(format, args...))
+	if isShowLogger {
+		println(fmt.Sprintf(format, args...))
+	}
 }
 func (l *defaultLogger) WarnCtxf(ctx context.Context, format string, args ...interface{}) {
-	println(fmt.Sprintf(format, args...))
+	if isShowLogger {
+		println(fmt.Sprintf(format, args...))
+	}
 }
 func (l *defaultLogger) ErrorCtxf(ctx context.Context, format string, args ...interface{}) {
-	println(fmt.Sprintf(format, args...))
+	if isShowLogger {
+		println(fmt.Sprintf(format, args...))
+	}
 }
 
 func SetLogger(log ILogger) {
 	logger = log
+}
+
+func ShowDefaultLogger(on bool) {
+	isShowLogger = on
 }
 
 func GetLogger() ILogger {
